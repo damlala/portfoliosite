9cd26e76d1da38e785c7942661090dc502488a37
Damla Öztürk <168738120+damlala@users.noreply.github.com>
2026-04-23 20:21:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function showPage(id) {
+function showPage(id, el) {
   const pages = document.querySelectorAll('.page');
+  const buttons = document.querySelectorAll('nav button');
 
-  pages.forEach(page => {
-    page.classList.remove('active');
-  });
-
+  pages.forEach(page => page.classList.remove('active'));
   document.getElementById(id).classList.add('active');
+
+  buttons.forEach(btn => btn.classList.remove('active-btn'));
+  el.classList.add('active-btn');
 }
